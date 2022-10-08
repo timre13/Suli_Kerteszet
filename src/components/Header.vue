bf81723aadf187a8793b09b0dc8994f28e687f82
0324bence <20050324bence@gmail.com>
2022-10-08 23:06:04 +0200

    Header finished

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,9 @@
             </RouterLink>
         </div>
         <div id="navbox">
-            <div id="title"></div>
+            <div id="title">
+                <span>{{ currentRoute.meta.title }}:</span>
+            </div>
             <div id="link">
                 <RouterLink to="/contacts">Elérhetőség</RouterLink>
             </div>
@@ -15,7 +17,10 @@
 </template>
 
 <script setup lang="ts">
-    import {RouterLink} from "vue-router";
+    import { RouterLink } from "vue-router";
+    import router from "@/router";
+
+    const currentRoute = router.currentRoute;
 </script>
 
 <style scoped lang="scss">
@@ -28,8 +33,9 @@
         #titlebox {
             display: grid;
             place-items: center;
-            
-            a, h1 {
+
+            a,
+            h1 {
                 text-decoration: none;
                 color: $title-color;
                 font-size: 4rem;
@@ -39,6 +45,37 @@
         #navbox {
             background-color: $secondary-accent-bg-color;
             display: flex;
+            align-items: stretch;
+
+            #title {
+                flex: 1 1;
+                display: flex;
+                align-items: center;
+                padding: 0 1rem 0 2rem;
+
+                span {
+                    font-size: 1.9rem;
+                    font-weight: bold;
+                }
+            }
+
+            #link {
+                flex: 1 0;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                justify-content: flex-end;
+                box-sizing: border-box;
+                padding: 0 2rem 0 2rem;
+                font-size: 1.3rem;
+                a {
+                    color: $secondary-font-color;
+                    text-decoration: none;
+                }
+                .router-link-active {
+                    text-decoration: underline;
+                }
+            }
         }
     }
 </style>
