dba359e2a98e857170c017462b8b887e0156fb29
0324bence <20050324bence@gmail.com>
2022-10-10 19:20:37 +0200

    Mobile menu half done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,9 +9,20 @@
             <div id="title">
                 <span>{{ currentRoute.meta.title }}{{ currentRoute.meta.title ? ":" : "" }}</span>
             </div>
-            <div id="link">
+            <div v-if="windowWidth > 450" id="link">
                 <RouterLink to="/contacts">Elérhetőség</RouterLink>
             </div>
+            <div v-else id="menu-container">
+                <div id="menu" @click="menuIsOpen = !menuIsOpen">
+                    <MenuIcon class="icon" />
+                </div>
+            </div>
+        </div>
+
+        <div id="menu-content" v-if="menuIsOpen">
+            <div class="item"><span>Elérhetőség</span></div>
+            <div class="item"><span>Elérhetőség</span></div>
+            <div class="item"><span>Elérhetőség</span></div>
         </div>
     </div>
 </template>
@@ -19,8 +30,20 @@
 <script setup lang="ts">
     import { RouterLink } from "vue-router";
     import router from "@/router";
+    import { onMounted, ref } from "vue";
+    import MenuIcon from "./MenuIcon.vue";
 
     const currentRoute = router.currentRoute;
+
+    const windowWidth = ref(window.innerWidth);
+
+    const menuIsOpen = ref(false);
+
+    onMounted(() => {
+        window.onresize = () => {
+            windowWidth.value = window.innerWidth;
+        };
+    });
 </script>
 
 <style scoped lang="scss">
@@ -28,7 +51,22 @@
         flex: 1 0 20vh;
         background-color: $secondary-bg-color;
         display: grid;
-        grid-template-rows: 2fr 1fr;
+        grid-template-rows: 2fr 1fr auto;
+
+        #menu-content {
+            display: flex;
+            flex-direction: column;
+            align-items: stretch;
+
+            .item {
+                padding: 1rem;
+                border-bottom: 1px solid $secondary-accent-bg-color;
+                display: grid;
+                place-items: center;
+                font-size: 2rem;
+                color: $secondary-font-color;
+            }
+        }
 
         #titlebox {
             display: grid;
@@ -39,6 +77,7 @@
                 text-decoration: none;
                 color: $title-color;
                 font-size: 4rem;
+                text-align: center;
             }
         }
 
@@ -56,6 +95,33 @@
                 span {
                     font-size: 1.9rem;
                     font-weight: bold;
+                }
+            }
+
+            #menu-container {
+                flex: 1 0;
+                display: flex;
+                justify-content: end;
+                align-items: center;
+                box-sizing: border-box;
+                padding: 1rem;
+
+                #menu {
+                    border: 2px solid black;
+                    box-sizing: border-box;
+                    padding: 0.3rem;
+                    display: grid;
+                    place-items: center;
+                    border-radius: 0.3rem;
+                    aspect-ratio: 1 / 1;
+                    cursor: pointer;
+                    &:active {
+                        transform: translate(1px, 2px);
+                    }
+                    .icon {
+                        width: 1.5rem;
+                        pointer-events: none;
+                    }
                 }
             }
 
