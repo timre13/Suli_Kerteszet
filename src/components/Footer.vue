fb5d1814c4f4ade7121db4e02f67724ec52ee919
0324bence <20050324bence@gmail.com>
2022-10-10 19:05:16 +0200

    Footer responsivity

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,15 @@
 <template>
     <div id="footer">
-        <div class="footer-textbox" id="phone">
-            <span>Tel. szám: <a href="tel:06307FOREST">06 30 7 FOREST</a></span>
+        <div class="flex">
+            <div class="footer-textbox" id="phone">
+                <span>Tel. szám: <a href="tel:06307FOREST">06 30 7 FOREST</a></span>
+            </div>
+            <div class="footer-textbox" id="email">
+                <span>E-mail: <a href="mailto:forest@garden.hu">forest@garden.hu</a></span>
+            </div>
         </div>
         <div class="footer-textbox" id="copyright">
             <span>Copyright © Forest Garden 2022</span>
-        </div>
-        <div class="footer-textbox" id="email">
-            <span>E-mail: <a href="mailto:forest@garden.hu">forest@garden.hu</a></span>
         </div>
     </div>
 </template>
@@ -16,27 +18,35 @@
     #footer {
         flex: 1 0 20vh;
         background-color: $secondary-bg-color;
-        display: flex;
+        display: grid;
+        grid-template-rows: 3fr 1fr;
         align-items: stretch;
         box-sizing: border-box;
         padding: 0.5rem;
         padding-top: 2rem;
         font-size: 1.1rem;
 
-        .footer-textbox {
-            flex: 1 0;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
+        #copyright {
+            text-align: center;
+        }
 
+        .footer-textbox {
             &,
             & a {
                 color: $secondary-font-color;
             }
         }
 
-        #copyright {
-            justify-content: end;
+        .flex {
+            display: flex;
+            justify-content: space-around;
+            flex-wrap: wrap;
+            .footer-textbox {
+                // flex: 1 0;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+            }
         }
     }
 </style>
